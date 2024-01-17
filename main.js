69d29590608938ce2e417f705fa54886f549bfcc
Gurveerbanvait <145047791+Gurveerbanvait@users.noreply.github.com>
2024-01-17 10:57:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  let num = +document.getElementById("input").value;
+  if (num === 1) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4475.jpeg" alt="d" />`;
+  } else if (num === 2) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4478.jpg" alt="d" />`;
+  } else if (num === 3) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4477.jpg" alt="d" />`;
+  } else if (num === 4) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4476.jpg" alt="" />`;
+  } else if (num === 5) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4481.jpg" alt="" />`;
+  } else if (num === 8) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4480.jpg" alt="" />`;
+  } else if (num === 9) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4479.jpg" alt="" />`;
+  } else if (num === 10) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4484.jpg" alt="" />`;
+  } else if (num === 15) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4483.jpg" alt="" />`;
+  } else if (num === 16) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4482.jpg" alt="" />`;
+  } else if (num === 17) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4487.jpg" alt="" />`;
+  } else if (num === 18) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4486.jpg" alt="" />`;
+  } else if (num === 19) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4485.jpg" alt="" />`;
+  } else if (num === 21) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4489.jpg" alt="" />`;
+  } else if (num === 22) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4492.jpg" alt="" />`;
+  } else if (num === 28) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4488.jpg" alt="" />`;
+  } else if (num === 23) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4490.jpg" alt="" />`;
+  } else if (num === 24) {
+    document.getElementById(
+      "dv"
+    ).innerHTML = `<img src="img/IMG_4491.jpg" alt="" />`;
+  }
+}
